3ce56a596b415186727a81315f9421ada21bb744
krystal gates <krystal_sabrina@yahoo.com>
2019-11-07 11:31:31 -0600

    update urls

--- src/components/contact/ContactDeleteModal.js
+++ src/components/contact/ContactDeleteModal.js
@@ -12,7 +12,7 @@ const ContactDeleteModal = props => {
 
   //Handles action of Deleting contact
   const deleteContact = () => {
-    fetch(`http://www.hellodanh.com/contacts/${props.contactId}`, {
+    fetch(`http://api.hellodanh.com/contacts/${props.contactId}`, {
       method: "DELETE",
       headers: {
         Authorization: `Token ${localStorage.getItem("helloDanh_token")}`
